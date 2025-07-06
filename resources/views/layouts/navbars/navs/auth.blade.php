82229aae70b2d700402142657f38a8765bdd3ca8
davi-scg <davi.gomes2@estudante.ifto.edu.br>
2025-07-06 13:19:12 -0300

    botao de search so aparece na pagina dashboard

--- resources/views/layouts/navbars/navs/auth.blade.php
+++ resources/views/layouts/navbars/navs/auth.blade.php
@@ -10,17 +10,21 @@
             </div>
             <a class="navbar-brand" href="#">{{ $page ?? __('Dashboard') }}</a>
         </div>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation"
+            aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-bar navbar-kebab"></span>
             <span class="navbar-toggler-bar navbar-kebab"></span>
             <span class="navbar-toggler-bar navbar-kebab"></span>
         </button>
         <div class="collapse navbar-collapse" id="navigation">
             <ul class="navbar-nav ml-auto">
-                <li class="search-bar input-group">
-                    <button class="btn btn-link" id="search-button" data-toggle="modal" data-target="#searchModal"><i class="tim-icons icon-zoom-split"></i>
-                    </button>
-                </li>
+                @if ($pageSlug == 'dashboard')
+                    <li class="search-bar input-group">
+                        <button class="btn btn-link" id="search-button" data-toggle="modal"
+                            data-target="#searchModal"><i class="tim-icons icon-zoom-split"></i>
+                        </button>
+                    </li>
+                @endif
                 <li class="dropdown nav-item">
                     <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
                         <div class="photo">
@@ -31,11 +35,13 @@
                     </a>
                     <ul class="dropdown-menu dropdown-navbar">
                         <li class="nav-link">
-                            <a href="{{ route('profile.edit', ['id' => Auth::id()]) }}" class="nav-item dropdown-item">{{ __('Profile') }}</a>
+                            <a href="{{ route('profile.edit', ['id' => Auth::id()]) }}"
+                                class="nav-item dropdown-item">{{ __('Profile') }}</a>
                         </li>
                         <li class="dropdown-divider"></li>
                         <li class="nav-link">
-                            <a href="{{ route('logout') }}" class="nav-item dropdown-item" onclick="event.preventDefault();  document.getElementById('logout-form').submit();">{{ __('Log out') }}</a>
+                            <a href="{{ route('logout') }}" class="nav-item dropdown-item"
+                                onclick="event.preventDefault();  document.getElementById('logout-form').submit();">{{ __('Log out') }}</a>
                         </li>
                     </ul>
                 </li>
@@ -44,13 +50,15 @@
         </div>
     </div>
 </nav>
-<div class="modal modal-search fade" id="searchModal" tabindex="-1" role="dialog" aria-labelledby="searchModal" aria-hidden="true">
+<div class="modal modal-search fade" id="searchModal" tabindex="-1" role="dialog" aria-labelledby="searchModal"
+    aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <form action="{{route('noticia.search')}}" method="get" class="form-search">
+                <form action="{{ route('noticia.search') }}" method="get" class="form-search">
                     @csrf
-                    <input type="text" name="search" class="input-search" id="inlineFormInputGroup" placeholder="{{ __('Pesquise pelo título da notícia') }}">
+                    <input type="text" name="search" class="input-search" id="inlineFormInputGroup"
+                        placeholder="{{ __('Pesquise pelo título da notícia') }}">
                     <button type="submit" class="button-search">
                         <i class="tim-icons icon-zoom-split"></i>
                     </button>
